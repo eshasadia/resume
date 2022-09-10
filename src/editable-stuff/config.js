1a257d1868e19413d1869b464d5a149150f71467
Esha Sadia <45667822+eshasadia@users.noreply.github.com>
2022-09-10 16:17:03 +0500

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,18 +6,18 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
+  firstName: "Esha",
   middleName: "",
-  lastName: "Shoaib",
+  lastName: "Sadia",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/eshasadia",
     },
     {
       image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
+      url: "https://www.facebook.com/eshasadianasir",
     },
     {
       image: "fa-instagram",
